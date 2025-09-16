77b79da532170ac2c018ef6907f292eb394e25c6
fu3nori <cap.fuminori.usui@gmail.com>
2025-09-16 13:05:36 +0900

    利用規約変更

--- client/src/pages/Legal.jsx
+++ client/src/pages/Legal.jsx
@@ -25,10 +25,10 @@ function Legal() {
                 </li>
                 <li className="title">責任の所在1:当サイトを利用して発生した物理的・精神的・金銭上のトラブルについて当サイトは一切責任を負いません。</li>
                 <li className="title">責任の所在2:当サイトは事故・災害・運営者の疾病または負傷などの理由で運営継続が困難になった場合サービスを事前に予告の上停止する事がございます</li>
+                <li className="title">補足事項:当サイトの利用規約は予告なく変更される事がございます。</li>
             </ul>
             <h3 className="title">利用料金</h3>
-            <p className="title">当サイトは非営利での利用のみ利用できます。<br />法人・個人事業主・その他営利目的でのご利用の際は商用利用規約(年10,000円でご契約頂きます。<br />
-                契約を希望される方は<i>hasidume.factory@gmail.com</i>までご連絡下さい)</p>
+            <p className="title">当サイトは無料で利用できます。</p>
             <h3 className="title">特定商取引法の表示</h3>
             <p className="title">運営者：橋詰史典<br />メールアドレス：hasidume.factory@gmail.com
             <br />その他の省略された情報の開示を希望される方は上記メールアドレスにお問い合わせされる方の本名・住所・利用目的を添えてお問い合わせください。
